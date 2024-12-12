3b849340e136cb017d4b6e2d721bac93fb4da1df
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-12-12 17:48:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,14 @@
 
 # setting wallpaper 
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s "/path/to/your/wallpaper.jpg"
+
+# setting tema aplikasi 
+xfconf-query -c xsettings -p /Net/ThemeName -s "NamaTema"
+
+# setting tema icon
+xfconf-query -c xsettings -p /Net/IconThemeName -s "NamaTemaIcon"
+
+# setting window manager 
+xfconf-query -c xfwm4 -p /general/theme -s "NamaTemaWindowManager"
+
+
